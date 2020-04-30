83fbe76bff7e0c90ccefbf67d361b4a49fc5ef3c
Devcoinfet <34756993+devcoinfet@users.noreply.github.com>
2020-04-30 13:48:07 -0700

    Update mqtt_dump.py

--- mqtt_dump.py
+++ mqtt_dump.py
@@ -1,46 +1,42 @@
 import paho.mqtt.client as mqtt
 import json
-import uuid
-import os
-import sys
+from signal import signal, SIGINT
+from sys import exit
+import codecs
+
+results_file = open("new.txt","w")
 
 
-fileout_json = open("mqtt.txt","a")
-clnt_id = uuid.uuid4()
-target = 'ipgoeshere'#sys.argv[1]
 
-
+def handler(signum, frame):
+    print('Ctrl+C pressed exiting gracefully still')
+    results_file.flush()
+    results_file.close()
+    exit(0)
 
 def on_connect(client, userdata, flags, rc):
-    print ("[+] Connection successful")
-    client.subscribe('#', qos = 1)        # Subscribe to all topics
-    client.subscribe('$SYS/#')   # Broker Status (Mosquitto)
+   print("[+] Connection successful")
+   client.subscribe('#', qos = 1)        # Subscribe to all topics
+   client.subscribe('$SYS/#')            # Broker Status (Mosquitto)
 
 def on_message(client, userdata, msg):
-    try:
-       if "$SYS" not in msg.topic:
-           print ('[+] Topic: %s - Message: %s' % (msg.topic, msg.payload))
-           topic = {}
-           topic['topic']= msg.topic
-           topic['payload']= msg.payload
-           final_info = json.dumps(topic)
-           fileout_json.write(final_info+"\n")
-       else:
-           pass
-    except:
-       pass
+   #print('[+] Topic: %s - Message: %s' % (msg.topic, msg.payload))
+   msg_out = {}
+   msg_out['msg_topic'] = str(msg.topic)
+   msg_out['msg_payload'] = str(msg.payload)
+   print(json.dumps(repr(msg_out)))
+   results_file.write(json.dumps(msg_out)+"\n")
+   results_file.flush()
+   
+   
 
-def dump_mqtt(host):
-    client = mqtt.Client(client_id =str(clnt_id))
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(host, 1883, 60)
-    client.loop_forever()
-
-def main():
-    try:
-       dump_mqtt(target)
-    except:
-       pass
-
-main()
+if __name__ == '__main__':
+   signal(SIGINT, handler)
+  
+   client = mqtt.Client(client_id = "MqttClient")
+   client.on_connect = on_connect
+   client.on_message = on_message
+   client.connect('ipaddress', 1883, 60)
+ 
+   
+   client.loop_forever()
